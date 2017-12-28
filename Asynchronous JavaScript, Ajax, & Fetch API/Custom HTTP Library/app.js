d102751322c42c6c5aeb8a57fec155894eff6b0e
Jacob Bloemker <jbloemker97@gmail.com>
2017-12-28 09:57:46 -0500

    Worked on promises and Fetch API

--- Asynchronous JavaScript, Ajax, & Fetch API/Custom HTTP Library/app.js
+++ Asynchronous JavaScript, Ajax, & Fetch API/Custom HTTP Library/app.js
@@ -1,7 +1,51 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get("https://jsonplaceholder.typicode.com/posts", function(err, response){
+http.get("https://jsonplaceholder.typicode.com/posts", function(err, post){
+    if(err){
+        console.log(err);
+    }else {
+        console.log(post);
+    }
+});
+
+// Get Single Post
+http.get("https://jsonplaceholder.typicode.com/posts/1", function(err, post){
+    if(err){
+        console.log(err);
+    }else {
+        console.log(post);
+    }
+});
+
+
+// Get Post Request 
+// Create Data
+const data = {
+    title: "Custom Post",
+    body: "This is a custom post"
+};
+
+// Create Post
+http.post("https://jsonplaceholder.typicode.com/posts", data, function(err, post){
+    if(err){
+        console.log(err);
+    }else {
+        console.log(post);
+    }
+});
+
+// Get PUT Request
+http.put("https://jsonplaceholder.typicode.com/posts/1", data, function(err, post){
+    if(err){
+        console.log(err);
+    }else {
+        console.log(post);
+    }
+});
+
+// Get DELETE Request 
+http.delete("https://jsonplaceholder.typicode.com/posts/2", function(err, response){
     if(err){
         console.log(err);
     }else {
