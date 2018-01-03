c08df6cd592ad6ddac9d8f1bb1fc3f623110586f
Jacob Bloemker <jbloemker97@gmail.com>
2018-01-03 16:43:49 -0500

    completed github app

--- Projects Folder/Github Finder/app.js
+++ Projects Folder/Github Finder/app.js
@@ -1,11 +1,31 @@
-// Search Input
-const searchUser = document.getElementById("searchUser");
+// Init Github
+const github = new Github;
+// Init UI
+const ui = new UI;
 
-searchUser.addEventListener("keyup", (e) => {
-    // Get input text
-    const userText = e.target.value;
+// Search input
+const searchUser = document.getElementById('searchUser');
 
-    if(userText !== ""){
-        console.log(userText);
-    }
-});
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+}); 
