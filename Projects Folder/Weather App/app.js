1297958f781198f2084a7a25857603496760318d
Jacob Bloemker <jbloemker97@gmail.com>
2018-01-04 17:01:00 -0500

    Finished weather app

---
+++ Projects Folder/Weather App/app.js
@@ -0,0 +1,29 @@
+// Init weather object
+const weather = new Weather("Daytona Beach", "FL");
+// Init UI
+const ui = new UI();
+
+// Get weather on DOM load
+document.addEventListener("DOMContentLoaded", getWeather);
+
+// Change location event
+document.getElementById("w-change-btn").addEventListener("click", (e) => {
+    const city = document.getElementById("city").value;
+    const state = document.getElementById("state").value;
+
+    weather.changeLocation(city, state);
+
+    // Get and display weather
+    getWeather();
+
+    // Cloase modal
+    $("#locModal").modal("hide");
+});
+
+function getWeather() {
+    weather.getWeather()
+    .then(results => {
+        ui.paint(results);
+    })
+    .catch(err => console.log(err))
+}
